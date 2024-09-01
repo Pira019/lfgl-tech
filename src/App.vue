9724bba5890b6c72f7022e3d6cd9bc6b0672e845
pires lufungula <pires00243@gmail.com>
2024-09-01 02:51:49 -0400

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorl>
+    <button class="btn btn-primary">TEST</button>
+  </HelloWorl>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+ 
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
+const path=require('path'); 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve :{
+    alias :{
+      '@' : path.resolve(__dirname,'./src'),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }},
   plugins: [vue()],
-})
+   
+   
+});
